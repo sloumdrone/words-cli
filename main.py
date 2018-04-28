23d0833b45eb7be2faaedd10da1ca5691f223e96
sloumdrone <brian.michael.evans@gmail.com>
2018-04-28 09:55:41 -0700

    First commit with import and class

---
+++ main.py
@@ -0,0 +1,23 @@
+import urllib, urllib2
+import argparse
+
+#http://www.datamuse.com/api/
+
+class Lookup:
+    def __init__(self):
+        self.word = word
+        self.type = type
+        self.options = options.split()
+
+
+    def call(self):
+        url = 'api.datamuse.com/words'
+        data = {
+            'type' : self.type
+        }
+
+
+
+
+words soundslike business
+words rhymeswith
